cea112ef98e183ccf485d0f8486324661ca29b07
Akhilesh Yadav <aksyvakin@gmail.com>
2019-03-08 20:07:39 +0530

    infix-postfix phase1

--- Practicals/infix-postfix.cpp
+++ Practicals/infix-postfix.cpp
@@ -1,18 +1,43 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 
+// A+(B*C-(D/E^F)*G)*H
+// Operands could be numbers,alphabets
+//  Symbols: +- , */ , * , ()
+
+template <typename T>
+void PrintStack(stack<T> s){
+
+	while(!s.empty()){
+		cout<<"\t "<<s.top();
+		s.pop();
+	}
+
+	cout<<"\n";
+
+}
 
 
 int main(){
 	
 	string s = "A+B";
 
+	stack<char> inputData;
+	stack<char> outputData;
+
+	string temp = "";
+
 	int i = 0;
 	for(i = 0;s[i]!='\0';i++){
-		
-		cout<<s[i];
 
+		inputData.push(s[i]);
+		//cout<<temp;
 	}
+
+	
+	PrintStack(inputData);
+
 
 	return 0;
 }
