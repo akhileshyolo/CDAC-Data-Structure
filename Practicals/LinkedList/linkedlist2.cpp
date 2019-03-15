38be1e13cc119aa172e034b7a2dbc765b039c989
Akhilesh Yadav <aksyvakin@gmail.com>
2019-03-15 13:10:41 +0530

    Linked List completed

--- Practicals/LinkedList/linkedlist2.cpp
+++ Practicals/LinkedList/linkedlist2.cpp
@@ -8,63 +8,138 @@
 #include<iostream>
 using namespace std;
 
-struct node{
+#define EXIT 0
 
-	int roll;
-	
+struct node{
+	string name;
 	struct node *next;
 
 };
 
 
-void insert(int roll, struct node **temp){
+void insert_front(struct node **temp){
 
 	struct node *p = (struct node *)malloc(sizeof(struct node));
-	p->roll = roll;
-
-	if(*temp==NULL){
-		p->next = NULL;
-
-	}else{
-		p->next = (*temp)->next;
-	}
-	*temp = p;
-
-
-	//cout<<p->roll<<" - \t Head at "<<*temp<<" , Next Addr  "<<(*temp)->next;
 	
+	if(p){
+	
+	   p->name = "viki";
+
+	   if((*temp)==NULL){
+		p->next = NULL;
+ 	   }else{
+		p->next = (*temp)->next;
+	   }
+	  
+	   *temp = p;
+
+	}
 
 }
 
-void show(struct node **temp){
 
-	while((*temp)->next!=NULL){
-		cout<<(*temp)->roll<<"\t";
+void insert_back(struct node **temp){
+
+	struct node *p = (struct node *)malloc(sizeof(struct node));
+	
+	if(p){
+	
+	   p->name = "akhilesh";
+
+	   if(*temp==NULL){
+		p->next = NULL;
+ 	   }else{
+		p->next = (*temp)->next;
+	   }
+	  
+	   *temp = p;
 
 	}
+
+}
+
+void display_all(struct node *temp){
+
+	int i = 0;
+	while((temp)){
+		cout<<temp->name<<"\t";
+		temp = temp->next;
+		i++;
+	}
+
+	cout<<i;
 }	
+
+void delete_front(struct node **temp){
+
+}
+
+void delete_back(struct node **temp){
+
+}
+
+
+void execute(int action, struct node **head){
+
+
+
+}
 
 
 int main(){
 
 	struct node *head,*temp;
-	int number;
-
-	cout<<"Enter no.of Students to enter data:";
-	cin>>number;
+	int user_input, action;
 
 	head = NULL;	
+	
+	while(true){
 
-	int i=0;
-	while(i<number){
-		
-		insert(i*10, &head);
-		i++;
-		//cout<<"\n Inside While loop , head at "<<head;
-		//break;
+		cout<< R"(\n
+			1. Insert at Front.
+			2.Insert at Back.
+			3.Delete Front.
+			4.Delete Back.
+			5.Display All.
+			0.Exit.
+			)";
+		cin>>action;
+
+		if(action==EXIT)
+			break;
+		else{
+			//execute(action, &head);
+			//
+
+			switch(action){
+	
+				case 1: insert_front(&head);
+					break;
+				/*
+				case 2: insert_back(&head);
+					break;
+				case 3: delete_front(&head);
+					break;
+				case 4: delete_back(&head);
+					break;
+				*/
+				case 5: display_all(head);
+					break;
+
+				default: cout<<"\n Invalid Option.";
+
+	
+			}
+
+
+
+
+
+		}
+
+		cout<<"\n Sabka head = "<<head->name;
+
 	}
 
-	show(&head);
-	
 	return 0;
 }
